81ebc005099386a021f49ee96747424f6cd9edc5
neatore <66874914+Dwk0910@users.noreply.github.com>
2025-08-31 00:42:51 +0900

    Test: Test commit

--- App.tsx
+++ App.tsx
@@ -5,41 +5,25 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import { Dimensions, Text, ViewStyle } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
+      <SafeAreaProvider>
+          <SafeAreaView style={styles.view}>
+              <Text>Hello, World!</Text>
+          </SafeAreaView>
+      </SafeAreaProvider>
   );
 }
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+const styles: { view: ViewStyle } = {
+    view: {
+        flex: 1,
+        width: Dimensions.get("window").width,
+        alignItems: "center"
+    },
+};
 
 export default App;
